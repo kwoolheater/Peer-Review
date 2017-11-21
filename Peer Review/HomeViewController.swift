a5155f646b3efe1482c32fa9ae14db90083cb1a9
Kiyoshi Woolheater <kiyoshiwoolheater@gmail.com>
2017-11-21 12:21:43 -0500

    feat: Add label to story board and connect them to view controller files

---
+++ Peer Review/HomeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Peer Review
+//
+//  Created by Kiyoshi Woolheater on 11/20/17.
+//  Copyright © 2017 Kiyoshi Woolheater. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    // initialize outlets
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var firstStar: UIImageView!
+    @IBOutlet weak var secondStar: UIImageView!
+    @IBOutlet weak var thirdStar: UIImageView!
+    @IBOutlet weak var fourthStar: UIImageView!
+    @IBOutlet weak var fifthStar: UIImageView!
+    @IBOutlet weak var ratingLabel: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+}
+
