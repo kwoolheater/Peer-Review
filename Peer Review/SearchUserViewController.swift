43022db0fae9ca9565cc97af1d5053e6a4afee80
Kiyoshi Woolheater <kiyoshiwoolheater@gmail.com>
2017-11-23 23:13:42 -0500

    feat: Add search user view controller

---
+++ Peer Review/SearchUserViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SearchUserViewController.swift
+//  Peer Review
+//
+//  Created by Kiyoshi Woolheater on 11/23/17.
+//  Copyright © 2017 Kiyoshi Woolheater. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+class SearchUserViewController: UIViewController {
+    
+    
+    
+}
