ce1060f146c5ed0612dff87158bccdcf277f44ed
Kiyoshi Woolheater <kiyoshiwoolheater@gmail.com>
2017-11-25 16:31:09 -0500

    feat: Configure database and add firebase cloud function

--- Peer Review/SearchUserViewController.swift
+++ Peer Review/SearchUserViewController.swift
@@ -9,20 +9,32 @@
 import Foundation
 import Firebase
 
-class SearchUserViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate {
+class SearchUserViewController: UIViewController {
     
     // declare outlets
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var searchBar: UISearchBar!
     
+    // declare variables
+    var ref: DatabaseReference!
+    var dataArray: [DataSnapshot]! = []
+    var namesArray: [String]?
+    fileprivate var _refHandle: DatabaseHandle!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        tableView.delegate = self
-        tableView.dataSource = self
-        searchBar.delegate = self
+        // tableView.delegate = self
+        // tableView.dataSource = self
+        // searchBar.delegate = self
+        configureDatabase()
     }
     
-    
+    func configureDatabase() {
+        ref = Database.database().reference()
+        _refHandle = ref.child("users").queryOrderedByKey() { (snapshot: DataSnapshot) in
+            self.dataArray?.append(snapshot)
+        }
+        print(dataArray)
+    }
     
 }
